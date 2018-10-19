41cb58d28e86e62af772364c4a5bb1f6f6151e2d
Austin R. Scott <ars@austinrscott.com>
2018-10-19 08:18:21 -0700

    Created science.py which actually has no Python code, but explains the core ideas behind this project.

---
+++ science.py
@@ -0,0 +1,71 @@
+# Contains the rules for the game's sciences.
+
+# There are 3 types of "unit" in the world. Picture the little boxes which comprise the Tetris pieces. In Tetris, all
+# of those little boxes are alike, but in this game, those little boxes come in 3 varieties.
+# Positive, negative, and neutral are the varieties of these little boxes, called Bits.
+
+# The Tetris-like shapes which represent the minerals of the world are called Tets. Prefixed words such as 1-tet, 2-tet,
+# 3-tet, ... and so on are used to describe the number of bits within a tet. Tets don't form larger than 9-tets; it
+# is a physical limit of the universe.
+
+# Neutral bits can form any tet, and can link with both positive and negative bits alike. But as for positive
+# and negative bits, they follow the rules of magnetism: like repels like, and opposites attract. Thus, positive
+# bits neighboring negative bits possess a stronger-than-typical attraction force between them and bits of
+# similar charge (excepting neutral of course) experience a strong repelling force when they are neighbors, and will not
+# form a tet (but if this repelling polarity is introduced while the bit is joined to a tet, it may be physically pre-
+# -vented from breaking off, and will be pressure-loaded for a later reaction).
+
+# Every tet has different qualities. Some of them are determined based upon its bit composition and structure, but some
+# are randomized using the tet's unique footprint as a random seed.
+
+# The game operates at multiple levels simultaneously. On the microscopic level, bits form tets and the configurations
+# and reactions of those tets create new materials if the configuration/reaction is stable, such as metals, alloys,
+# corrosives, beam modifiers (mirrors, resonators, amplifiers, etc), etc. and effects (if the configuration/reaction is
+# unstable) such as explosions, beams, light/radio discharge, incendiaries, etc.
+
+# Tets are gathered in bulk from the walls of the caverns in which this game takes place. The cavern walls are a blend
+# of many different types of tet, and this resource availability is determined based upon some procedural generation.
+# In the beginning, the player has no choice but to run to the resources with a container, manually collect them, haul
+# the bulk ore back to their base and manually run them through the Materials Processor Mark I. Materials Processor
+# components can perform actions at the tetular level, and can be used for extraction, transmutation, and experiments.
+# One of them is always available to the player, included in their base tool set.
+
+# Extraction is like a reverse Tetris game. The player has to free the tets from the bulk gathered material using
+# different methods. In the beginning, the methods are crude. The larger tets are rarer, but require much delicate work
+# to extract them unharmed. They also trap more neighboring tets, being larger.
+# Heating is the first method to extract tets. The first level of heat makes 9-tets with no polarity or polarized neigh-
+# -bors become released. This state is called 'slippery.' Then, as the heat rises to another level, 8-tets become slip-
+# -pery and 9-tets become depolarized. At the third level of heat, 7-tets become slippery, 8-tets become depolarized,
+# and 9-tets break into smaller tets. This pattern continues all the way until every tet is released from the bulk grab.
+
+# Tet extraction is a metaphoric minigame for the actual process, and a large amount of material is gathered from this
+# process even though the visualization is on the tetular level. It does take in-game time to process, however, which
+# eventually will become a factor when processing is automated.
+
+# When the tets are gathered in quantities, they may be used for samples in experiments (which uses up small amounts of
+# materials depending on the experiment parameters) and they may be used for component production. This requires that a
+# component design exists in addition to the required materials. Fabricators can be used to produce an item once all
+# of the necessary prerequisites are in place. One of these is included in the base set of tools.
+
+# On the next level of size up, blueprints are created to lay out combinations of components and housing. This is a
+# similar process with both machines and buildings. Construction can take place gradually, with blueprint modifications
+# and phases being implemented. Constructors are tools that create chassis for machines and for buildings create walls.
+# They also create linkage of various types and a few other accessories. The Constructor Mark I is manual and requires
+# the player to guide it.
+
+# On the construction level, components require linkage in order to perform their work. Mechanical components such as
+# wheels, arms, tools, and weapons require some sort of energy to perform their actions in addition to some form of
+# lubricant and sometimes ammo. Sensors, computer equipment and the like require electricity, data cables and cooling.
+# Engines require fuel. All of these links are created in construction and determine how the system performs.
+
+# Nearly all technologies are known by the player in the beginning, but the player doesn't have the necessary materials
+# research to utilize them. In other words, the idea for a drill bit exists already but the player must discover ways of
+# fulfilling the material requirements to build one. Very few technologies are discovered later as artifact finds by the
+# player.
+
+# Discovery of the materials and their properties simply requires the player gather bulk tets and play around with them
+# to unlock their new configurations. Once a desired material is produced, it may be put into production.
+
+# The larger tets are rarer and have more interesting effects. In addition to that, there are many, many combinations of
+# the larger tets. The smallest tets are very common minerals which are useful for general applications. In the activity
+# of mining tets, there will be oil, gas, and water resources discovered.
