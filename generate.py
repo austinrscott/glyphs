c6864d63af38cb287ef3c9faeb27bc1daa147304
Austin R. Scott <ars@austinrscott.com>
2019-07-18 19:16:54 -0700

    Working on new glyph gen function

--- generate.py
+++ generate.py
@@ -1,39 +1,37 @@
 # This script generates all glyphs up to a known point.
 from glyph import Glyph
-
+import pyyaml
 
 def homogenize(g):
     a, b, c, d = g, g.rotated_90, g.rotated_90.rotated_90, g.rotated_90.rotated_90.rotated_90
-    selection = [a, b, c, d]
-    if not a.width == b.width == c.width == d.width:
+    a_h = a.flip_horizontal
+    b_h, c_h, d_h = a_h.rotated_90, a_h.rotated_90.rotated_90, a_h.rotated_90.rotated_90.rotated_90
+    a_w = a.flip_vertical
+    b_w, c_w, d_w = a_w.rotated_90, a_w.rotated_90.rotated_90, a_w.rotated_90.rotated_90.rotated_90
+    selection = {a, b, c, d, a_h, b_h, c_h, d_h, a_w, b_w, c_w, d_w}
+    if not (a.width == b.width == c.width == d.width):
         if a.width < b.width:
-            selection.remove(a)
-            selection.remove(c)
+            map(selection.remove, (a, c, a_h, c_h, a_w, c_w))
         else:
-            selection.remove(b)
-            selection.remove(d)
+            map(selection.remove, (b, d, b_h, d_h, b_w, d_w))
     representative = min(selection, key=lambda glyph: glyph.score)
-    return representative, frozenset(a.solids), frozenset(b.solids), frozenset(c.solids), frozenset(d.solids)
+    selection.remove(representative)
+    return representative, selection
 
 
 if __name__ == "__main__":
-    starter_glyph = Glyph()
-    unique_glyphs, all_glyphs, latest_generation = {starter_glyph}, {frozenset(starter_glyph.solids): starter_glyph}, {starter_glyph}
-    for i in range(3):
-        print(f'Generation: {i}')
-        new_glyphs = set()
-        while latest_generation:
-            parent = latest_generation.pop()
-            for o in parent.openings:
-                new_solids = parent.solids | {o}
-                new_g = Glyph(new_solids)
-                if not frozenset(new_solids) in all_glyphs.keys():
-                    result = homogenize(new_g)
-                    representative, all_glyph_forms = result[0], result[1:]
-                    unique_glyphs |= {representative}
-                    for frozen_solid in all_glyph_forms:
-                        all_glyphs[frozen_solid] = representative
-                    new_glyphs |= {new_g}
-
-        print("New glyphs generated:", *new_glyphs, sep="\n", end="\n\n")
-        latest_generation |= new_glyphs
+    memo = {}
+    generations = 0
+    this_generation = {Glyph()}
+    for i in range(generations + 1):
+        print(f"Gen {i} ", end="-" * 80 + "\n")
+        next_generation = {}
+        for glyph in this_generation:
+            rep, selection = homogenize(glyph)
+            m = {
+                'representative': rep,
+                'all_forms': selection | {rep}
+            }
+            for form in selection | {rep}:
+                memo[form] = m
+                next_generation |= form.children
--- glyph.py
+++ glyph.py
@@ -1,4 +1,6 @@
-DIRS = {
+from typing import Tuple, Set, Iterable, Dict, Optional
+
+DIRS: Dict[str, Tuple[int, int]] = {
     'up': (0, -1),
     'down': (0, 1),
     'left': (-1, 0),
@@ -8,47 +10,55 @@ DIRS = {
 
 class Glyph:
     @property
-    def score(self):
+    def class_(self) -> int:
+        return len(self.solids)
+
+    @property
+    def score(self) -> int:
         return sum(x + self.width * y for x, y in self.solids)
 
     @property
-    def width(self):
+    def width(self) -> int:
         return max(x for x, y in self.solids)
 
     @property
-    def height(self):
+    def height(self) -> int:
         return max(y for x, y in self.solids)
 
     @property
-    def center(self):
+    def center(self) -> Tuple[float, float]:
         return (self.width + 1) / 2, (self.height + 1) / 2
 
     @property
-    def solids(self):
+    def solids(self) -> Set[Tuple[int, int]]:
         try:
             return self._solids
         except AttributeError:
             return set()
 
     @property
-    def openings(self):
+    def openings(self) -> Set[Tuple[int, int]]:
         return {(x + i, y + j) for i, j in DIRS.values() for x, y in self.solids} - self.solids
 
+    @property
+    def children(self) -> Set['Glyph']:
+        return {Glyph(self.solids | {o}) for o in self.openings}
+
     @solids.setter
-    def solids(self, new_solids_list):
+    def solids(self, new_solids_list: Iterable[Tuple[int, int]]) -> None:
         x_offset, y_offset = 1 - min(x for x, y in new_solids_list), 1 - min(y for x, y in new_solids_list)
-        self._solids = {(x + x_offset, y + y_offset) for x, y in new_solids_list}
+        self._solids: Set[Tuple[int, int]] = {(x + x_offset, y + y_offset) for x, y in new_solids_list}
 
     @property
-    def rotated_90(self):
+    def rotated_90(self) -> 'Glyph':
         return Glyph({self._rotate_point_90(p) for p in self.solids})
 
-    def _convert_point_for_rotation(self, point):
+    def _convert_point_for_rotation(self, point: Tuple[int, int]) -> Tuple[float, float]:
         x, y = point
         i, j = self.center
         return x - i, y - j
 
-    def _revert_point_to_default_space(self, point, total_rotation=90):
+    def _revert_point_to_default_space(self, point: Tuple[int, int], total_rotation: int = 90) -> Tuple[int, int]:
         x, y = point
 
         # When reverting back to default space from a rotation, the center might not be updated yet. Therefore, this
@@ -60,18 +70,21 @@ class Glyph:
 
         return int(x + i), int(y + j)
 
-    def _rotate_point_90(self, point):
+    def _rotate_point_90(self, point: Tuple[int, int]) -> Tuple[int, int]:
         x, y = self._convert_point_for_rotation(point)
         return self._revert_point_to_default_space((-y, x))
 
-    def __init__(self, solids={(1, 1)}):
-        self.solids = solids
+    def __init__(self, solids: Optional[Set[Tuple[int, int]]] = None):
+        self.solids = solids if solids else {(1, 1)}
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         string = ''
         for j in range(self.height + 2):
             string += '\n'
             for i in range(self.width + 2):
-                string += '[]' if (i, j) in self.solids else '__'
+                string += chr(1) if (i, j) in self.solids else chr(183)
+            string += '\t\t\t'
+            for i in range(self.width + 2):
+                string += chr(1) if (i, j) in self.solids else chr(215) if (i, j) in self.openings else chr(183)
         string += f'\nScore: {self.score}'
         return string
